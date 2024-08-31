0c885f8fb7077d92eced43cc531447dcc710cf2a
Wouter Glorieux <wouter@valyrian.tech>
2024-08-31 10:33:20 +0200

    fix location of asset file

--- comfyui-with-flux/replaceDefaultGraph.py
+++ comfyui-with-flux/replaceDefaultGraph.py
@@ -9,12 +9,12 @@ with open('/defaultGraph.json', 'r') as json_file:
 json_string = json.dumps(json_data)
 
 # Load the JavaScript file
-with open('/ComfyUI/assets/index-CI3N807S.js', 'r') as js_file:
+with open('/ComfyUI/web/assets/index-CI3N807S.js', 'r') as js_file:
     js_data = js_file.read()
 
 # Locate the defaultGraph dictionary and replace it with the JSON string
 updated_js_data = re.sub(r'(?<=const defaultGraph = ).*?(?=;\nwindow\.comfyAPI)', json_string, js_data, flags=re.DOTALL)
 
 # Write the updated JavaScript data back to the file
-with open('/ComfyUI/assets/index-CI3N807S.js', 'w') as js_file:
+with open('/ComfyUI/web/assets/index-CI3N807S.js', 'w') as js_file:
     js_file.write(updated_js_data)
