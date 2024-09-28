5b777de92f4352e41541df19521cec92c36940db
Wouter Glorieux <wouter@valyrian.tech>
2024-09-28 21:04:38 +0200

    copy files from comfyui-with-flux

---
+++ comfyui-without-flux/start-ssh-only.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "pod started"
+
+if [[ $PUBLIC_KEY ]]
+then
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+    cd ~/.ssh
+    echo $PUBLIC_KEY >> authorized_keys
+    chmod 700 -R ~/.ssh
+    cd /
+    service ssh start
+fi
+
+# Move text-generation-webui's folder to $VOLUME so models and all config will persist
+/comfyui-on-workspace.sh
+
+# Start nginx as reverse proxy to enable api access
+service nginx start
+
+# Start JupyterLab
+jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.allow_origin='*' &
+echo "JupyterLab started"
+
+# Launch the UI
+python3 /workspace/ComfyUI/main.py --listen
+
+sleep infinity
