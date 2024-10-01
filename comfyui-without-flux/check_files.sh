c88bdd1107ceecfdbd06931112150604adf44777
Wouter Glorieux <wouter@valyrian.tech>
2024-10-01 19:32:23 +0200

    make the warning when flux is not present more visible

--- comfyui-without-flux/check_files.sh
+++ comfyui-without-flux/check_files.sh
@@ -3,10 +3,13 @@
 FILE="/workspace/models/diffusion_models/flux1-def.sft"
 
 if [ ! -f "$FILE" ]; then
-    echo "   WARNING!   "
+    echo "#################################################################"
+    echo "WARNING!"
+    echo "#################################################################"
     echo "File $FILE does not exist."
     echo "Some of the required files, like the flux.1 model, are not present."
     echo "This docker image is intended to be used in combination with a network volume that has been initialized with the 'ComfyUI WITH Flux' template first."
     echo "The 'ComfyUI WITHOUT Flux' container and 'ComfyUI WITH Flux' containers are identical, except that the 'ComfyUI WITHOUT Flux' container does not include about 20GB of model files."
     echo "Please use the 'ComfyUI WITH Flux' template first with a new and empty network volume, this will copy all the files to the network volume so that you don't need to download them again each time you deploy a new pod."
+    echo "#################################################################"
 fi
