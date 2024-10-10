b5fff8e77257a5c77c718067965a9a1d00e3f847
Wouter Glorieux <wouter@valyrian.tech>
2024-10-10 11:40:45 +0200

    prevent symlink loop

--- comfyui-without-flux/ai-toolkit-on-workspace.sh
+++ comfyui-without-flux/ai-toolkit-on-workspace.sh
@@ -16,4 +16,10 @@ ln -s /workspace/ai-toolkit /ai-toolkit
 
 # Ensure we have /workspace/training_sets in all scenarios
 mkdir -p /workspace/training_set
-ln -s /workspace/ai-toolkit/output /workspace/ComfyUI/models/loras/ai-toolkit
+mkdir -p /workspace/LoRas
+
+# when trained using the UI, the result is stored in /workspace/ai-toolkit/output
+ln -s /workspace/ai-tookit/output /workspace/ComfyUI/models/loras/flux_train_ui
+
+# when trained using the CLI, the result set is stored in /workspace/LoRas (don't put it in /workspace/ai-toolkit/output because it will create a symlink loop)
+ln -s /workspace/LoRas /workspace/ComfyUI/models/loras/ai-toolkit
