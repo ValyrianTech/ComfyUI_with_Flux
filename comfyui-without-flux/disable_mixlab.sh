7f4fbe9f129fc1fb08dea555f14588d6dcdaad86
Wouter Glorieux <wouter@valyrian.tech>
2024-10-12 18:48:31 +0200

    disable mixlab because it takes a long time to load and is no longer needed in any of the included workflows

---
+++ comfyui-without-flux/disable_mixlab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dir="workspace/ComfyUI/custom_nodes/comfyui-mixlab-nodes"
+
+if [ -d "$dir" ]
+then
+    mv "$dir" "$dir.disabled"
+    echo "Mixlab nodes has been disabled successfully."
+else
+    echo "Mixlab nodes not found."
+fi
--- comfyui-without-flux/start-original.sh
+++ comfyui-without-flux/start-original.sh
@@ -6,6 +6,10 @@
 # Update the included workflows
 bash /update_Workflows.sh
 
+# Disable Mixlab nodes because they take a long time load and are no longer needed in any of the included workflows.
+# But can be enabled if needed by commenting out the following line.
+bash /disable_mixlab.sh
+
 # Launch the UI
 python3 /workspace/ComfyUI/main.py --listen
 
