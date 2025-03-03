ade2f71e9c4923a28272afc875178b92b977ef43
Wouter Glorieux <wouter@valyrian.tech>
2025-03-03 11:16:50 +0100

    added message to indicate when data is copied to /workspace

--- comfyui-without-flux/comfyui-on-workspace.sh
+++ comfyui-without-flux/comfyui-on-workspace.sh
@@ -5,6 +5,7 @@ mkdir -p /workspace
 
 if [[ ! -d /workspace/ComfyUI ]]; then
 	# If we don't already have /workspace/ComfyUI, move it there
+	echo "Copying ComfyUI to /workspace for persistence, this might take a while"
 	mv /ComfyUI /workspace
 else
 	# otherwise delete the default ComfyUI folder which is always re-created on pod start from the Docker
