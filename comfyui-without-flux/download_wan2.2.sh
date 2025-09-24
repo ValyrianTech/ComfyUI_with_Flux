0724ffe500560b0de92d95b61dcca6961fb7969e
Wouter Glorieux <wouter@valyrian.tech>
2025-09-24 18:12:18 +0200

    added wan2.2 download script

---
+++ comfyui-without-flux/download_wan2.2.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+echo "Downloading umt5_xxl_fp8_e4m3fn_scaled.safetensors text encoders"
+cd /ComfyUI/models/text_encoders/
+
+file="umt5_xxl_fp8_e4m3fn_scaled.safetensors"
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
+
+
+echo "Downloading wan_2.1_vae.safetensors"
+cd /ComfyUI/models/vae/
+
+file="wan_2.1_vae.safetensors"
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
+
+echo "Downloading wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors"
+cd /ComfyUI/models/diffusion_models/
+
+file="wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors"
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
+
+echo "Downloading wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors"
+cd /ComfyUI/models/diffusion_models/
+
+file="wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors"
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
+
+echo "Downloading wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors"
+cd /ComfyUI/models/diffusion_models/
+
+file="wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors"
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
+
+echo "Downloading wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors"
+cd /ComfyUI/models/diffusion_models/
+
+file="wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors"
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
